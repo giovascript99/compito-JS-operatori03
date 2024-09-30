4b01e89d5302ae93565b0d241a045048ef974c08
Giovanni Perniola <gioperniola1999@gmail.com>
2024-09-30 17:57:33 +0200

    corretto selfwork

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let num1 = 9;
-let num2 = 3;
+let num1 = parseInt(prompt('Inserisci il primo numero'));
+let num2 = parseInt(prompt('Inserisci il secondo numero'));
 
 let somma = num1 + num2;
 let sottrazione = num1 - num2;
@@ -7,5 +7,8 @@ let moltiplicazione = num1 * num2;
 let divisione = num1 / num2;
 let potenza = num1 ** num2;
 
-console.log(`Con i numeri da me scelti, i risultati delle varie operazioni sono: somma (${somma}), sottrazione (${sottrazione}), moltiplicazione (${moltiplicazione}), divisione (${divisione}), potenza (${potenza})`);
+console.log('Con i numeri da te scelti, i risultati delle varie operazioni sono: ');
+
+console.log(`somma (${somma}), sottrazione (${sottrazione}), moltiplicazione (${moltiplicazione}), divisione (${divisione}), potenza (${potenza})`);
+
 
