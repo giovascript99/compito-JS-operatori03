b83565c024e37ab091148a066667bab202ccc072
Giovanni Perniola <gioperniola1999@gmail.com>
2024-09-30 17:48:39 +0200

    terminato selfwork operatori 3

---
+++ main.js
@@ -0,0 +1,11 @@
+let num1 = 9;
+let num2 = 3;
+
+let somma = num1 + num2;
+let sottrazione = num1 - num2;
+let moltiplicazione = num1 * num2;
+let divisione = num1 / num2;
+let potenza = num1 ** num2;
+
+console.log(`Con i numeri da me scelti, i risultati delle varie operazioni sono: somma (${somma}), sottrazione (${sottrazione}), moltiplicazione (${moltiplicazione}), divisione (${divisione}), potenza (${potenza})`);
+
